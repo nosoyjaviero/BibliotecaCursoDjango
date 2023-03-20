6cba518561cf0c61d5b0bdf5b67211c761834cd4
nosoyjaviero <lopez4935@gmail.com>
2023-03-20 03:01:38 -0600

    Managers 10.2  Rango de fechas

--- apps/libro/managers.py
+++ apps/libro/managers.py
@@ -9,7 +9,8 @@ class LibroManager(models.Manager):
     def buscar_libros(self, kword):
         #mientras que el segundo utiliza un Manager personalizado llamado "AutorManager" definido en la clase "AutorManager".        
         libros= self.filter(
-            titulo__icontains=kword
+            titulo__icontains=kword,
+            fecha_lanzamiento__range=('2000-01-01','2002-01-01')
         )
         return libros
     
