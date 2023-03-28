eaf9b4eebc36f570f4b92fafc0fe3b0a229dd8b3
nosoyjaviero <lopez4935@gmail.com>
2023-03-28 11:43:17 -0600

    Instalacion 2 Trigram, Postgree

--- biblioteca/settings/base.py
+++ biblioteca/settings/base.py
@@ -33,6 +33,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.postgres',
     #local apps
     #orden de dependecia
     
