0eea7ef9cb82aa9ecb245599b1f99bd44b7c06c0
nosoyjaviero <lopez4935@gmail.com>
2023-03-29 12:47:02 -0600

    Managers 17 Trigram 4. Creamos la url

--- apps/libro/urls.py
+++ apps/libro/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
      path("lista-libros", views.LibrosListView.as_view(), name="lista_libros"),
+     path("lista-libro2", views.LibrosListView_conTrigram.as_view(), name="lista_librosTrigram"),
      path("lista-2", views.FiltrarporCategoria.as_view(), name="lista_libros2"),
      path("libro-detalle/<pk>", views.LibroDetailView.as_view(), name="libro_detalle"),
      
